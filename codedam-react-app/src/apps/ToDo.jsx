b9d8594e5e9f024e4bfe5d944b6adb8e003cb147
devrathrana <devrath.rana@bold.com>
2025-03-17 23:09:00 +0530

    todo app done

--- codedam-react-app/src/apps/ToDo.jsx
+++ codedam-react-app/src/apps/ToDo.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from "react";
 
+let globalId = 0;
 const ToDo = () => {
-  return (
-    <div>ToDo</div>
-  )
-}
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([{ name: "task 1", id: ++globalId }]);
 
-export default ToDo
+  const handleTodoInput = (ev) => {
+    const inputValue = ev.target.value;
+    setTodo(inputValue);
+  };
+
+  const handleFormSubmit = (ev) => {
+    ev.preventDefault();
+    setTodos((prev) => [
+      ...prev,
+      { name: todo, id: prev[prev.length - 1].id + 1 },
+    ]);
+  };
+
+  const handleToDoDelete = (todoId) => {
+    setTodos(todos.filter((item) => item.id !== todoId));
+  };
+
+  return (
+    <div>
+      <h1>To Dos</h1>
+      <form onSubmit={handleFormSubmit}>
+        <input name="todo" value={todo} onChange={handleTodoInput} />
+        <input type="submit" value="Add to do" />
+      </form>
+      <ul>
+        {/* using array method as we can't write for loop or logical expressions inside jsx curly braces */}
+        {todos.map((toDoItem, toDoKey) => (
+          <li key={toDoKey}>
+            {toDoItem.name} ({toDoItem.id})
+            <button onClick={() => handleToDoDelete(toDoItem.id)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ToDo;
