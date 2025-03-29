cc303af6a7efafb72cc39e308b50064d734df3fd
devrathrana <devrath.rana@bold.com>
2025-03-29 16:42:35 +0530

    async-function-in-useEffect-component

--- src/apps/AsyncFunctionInUseeffectHooks.jsx
+++ src/apps/AsyncFunctionInUseeffectHooks.jsx
@@ -4,6 +4,7 @@ const AsyncFunctionInUseeffectHooks = () => {
   const [msg, setMsg] = useState("");
   useEffect(() => {
     // This following approach is not recommended
+    // By default async returns a promise so chant use this keyword for useEffect callback as it can only have one cleanup function being returned not another promise
     async function fetchData() {
       const data = await fetch("/data.json").then((reqData) => reqData.json());
       setMsg(data.name);
